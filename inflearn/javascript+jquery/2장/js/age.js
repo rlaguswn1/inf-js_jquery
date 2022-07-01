3d04e7a51ee33db33f8932edc8b87c287480ac4f
khj <khj9872100@gmail.com>
2022-07-01 17:32:30 +0900

    폴더정리

--- inflearn/javascript+jquery/2장/js/age.js
+++
@@ -1,6 +0,0 @@
-function calc() {
-    var currentYear = 2022;
-    var birthYear = prompt("태어난 연도를 입력하세요", "YYYY");
-    var age=currentYear-birthYear+1;
-    document.querySelector("#result").innerHTML = "당신의 나이는 " + age + "세입니다.";
-}
